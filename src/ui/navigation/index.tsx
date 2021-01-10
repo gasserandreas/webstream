eec06b6367c6dde588f891f1b2b5ea2aaee8ebbc
Andreas Gasser <gasserandreas@users.noreply.github.com>
2021-01-10 17:15:05 +0100

    feat(#32): improved navigation styles

--- src/ui/navigation/index.tsx
+++ src/ui/navigation/index.tsx
@@ -1,42 +1,27 @@
-import React, { FC, ReactNode, useState } from 'react';
+import React, { FC, ReactNode } from 'react';
 
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import clsx from 'clsx';
 
 type NavigationProps = {
   bottomElements?: Array<ReactNode>;
 };
 
-const useStyles = makeStyles((theme) => {
-  const navigationWidth = theme.spacing(7.25);
-  const hoverAreaWidth = theme.spacing(4);
+export const NAVIGATION_WIDTH = 40;
 
-  return createStyles({
-    wrapper: {
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    navigation: {
       position: 'fixed',
       top: 0,
+      left: 0,
       bottom: 0,
-      left: `-${navigationWidth}px`,
-      width: `${navigationWidth + hoverAreaWidth}px`,
+      width: `${NAVIGATION_WIDTH}px`,
       zIndex: 1,
-      display: 'flex',
-      flexFlow: 'row',
-      justifyContent: 'stretch',
-    },
-    hoverArea: {
-      width: `${hoverAreaWidth}px`,
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      cursor: 'pointer',
-    },
-    navigation: {
-      flexShrink: 1,
-      flexGrow: 1,
       backgroundColor: theme.palette.background.paper,
       display: 'flex',
       flexFlow: 'column',
       padding: '0.5rem 0.25rem',
-      width: `${navigationWidth}px`,
     },
     navigationContent: {
       flexGrow: 1,
@@ -49,45 +34,17 @@ const useStyles = makeStyles((theme) => {
     visible: {
       left: 0,
     },
-  });
-});
+  })
+);
 
 const Navigation: FC<NavigationProps> = ({ children, bottomElements }) => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [alwaysVisible, setAlwaysVisible] = useState<boolean>(false);
-
-  const classes = useStyles({
-    isVisible: isVisible || alwaysVisible,
-  });
-
-  const handleOnMouseEnter = () => {
-    setIsVisible(true);
-  };
-
-  const handleOnMouseLeave = () => {
-    setIsVisible(false);
-  };
-
-  const handleHoverClick = () => {
-    setAlwaysVisible((prev) => !prev);
-  };
-
-  const wrapperClassName = isVisible
-    ? clsx(classes.wrapper, classes.visible)
-    : classes.wrapper;
+  const classes = useStyles();
 
   return (
-    <div
-      className={wrapperClassName}
-      onMouseEnter={handleOnMouseEnter}
-      onMouseLeave={handleOnMouseLeave}
-    >
-      <Box className={classes.navigation}>
-        <Box className={classes.navigationContent}>{children}</Box>
-        <Box className={classes.navigationBottom}>{bottomElements}</Box>
-      </Box>
-      <Box className={classes.hoverArea} onClick={handleHoverClick} />
-    </div>
+    <Box className={classes.navigation} boxShadow="1">
+      <Box className={classes.navigationContent}>{children}</Box>
+      <Box className={classes.navigationBottom}>{bottomElements}</Box>
+    </Box>
   );
 };
 
